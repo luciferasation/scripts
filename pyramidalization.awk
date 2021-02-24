9238469bcb798b3d1684acbfd1f9d59889932c18
luciferasation <50685726+luciferasation@users.noreply.github.com>
2021-02-24 03:07:20 -0500

    Add files via upload
    A script to calculate pyramidalization angles on xyz files.

---
+++ pyramidalization.awk
@@ -0,0 +1,89 @@
+BEGIN {
+FS = " ";
+numPyramidalization = split(p,pyramidalization);
+maxserial = 0;
+for (i = 1; i <= numPyramidalization; i++)
+    maxserial = (maxserial > pyramidalization[i]) ? maxserial : pyramidalization[i];
+}
+
+# The following commands will only be executed on the 1st frame to extract ele-
+# ments.
+FNR == NR {
+if (FNR == 1)
+    numAtom = $1;
+else
+{
+    atomserial = FNR - 2;
+    if (atomserial <= maxserial)
+    {
+        if (atomserial in atom)
+            next;
+        else
+        for (i = 1; i <= numPyramidalization; i++)
+        {
+            if (atomserial == pyramidalization[i])
+            {
+                atom[atomserial] = $1;
+                break;
+            }
+        }
+        if (atomserial == maxserial)
+        {
+            numPyramidalization = numPyramidalization/4;
+            printf("#");
+            for (i = 1; i <= numPyramidalization; i++)
+            {
+                for (j=1;j<=4;j++)
+                    P[i,j] = pyramidalization[4*(i-1)+j];
+                printf("%s%d-%s%d-%s%d-%s%d ", atom[P[i,1]], P[i,1], atom[P[i,2]], P[i,2], atom[P[i,3]], P[i,3], atom[P[i,4]], P[i,4]);
+            }
+            printf("\n");
+            nextfile;
+        }
+    }
+}
+next;
+}
+
+# The following commands will be executed on all of the frames.
+NF == 4 {
+    atomserial = FNR % (numAtom + 2) - 2;
+    if (atomserial == -2)
+        atomserial = numAtom;
+    if (atomserial in atom)
+    {
+        A[atomserial,1] = $2;
+        A[atomserial,2] = $3;
+        A[atomserial,3] = $4;
+    }
+    if (atomserial == maxserial)
+    {
+        for (i = 1; i <= numPyramidalization; i++)
+            printf("%.2f ", thetap(P[i,1],P[i,2],P[i,3],P[i,4]));
+        printf("\n");
+    }
+}
+
+function cosangle(a1,a2,a3,    x1,y1,z1,x2,y2,z2,x3,y3,z3)
+{
+    x1 = A[a1,1];
+    y1 = A[a1,2];
+    z1 = A[a1,3];
+    x2 = A[a2,1];
+    y2 = A[a2,2];
+    z2 = A[a2,3];
+    x3 = A[a3,1];
+    y3 = A[a3,2];
+    z3 = A[a3,3];
+    return ((x1-x2)*(x3-x2)+(y1-y2)*(y3-y2)+(z1-z2)*(z3-z2))/sqrt(((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2))*((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)+(z3-z2)*(z3-z2)));
+}
+
+# The unit of the results given by function thetap is degree.
+function thetap(a1,a2,a3,a4,    cosalpha,cosbeta,cosgamma,sinthetap)
+{
+    cosalpha = cosangle(a2,a1,a3);
+    cosbeta = cosangle(a3,a1,a4);
+    cosgamma = cosangle(a4,a1,a2);
+    sinthetap = sqrt((2*cosalpha*cosbeta*cosgamma-cosalpha*cosalpha-cosbeta*cosbeta-cosgamma*cosgamma+1)/(-cosalpha*cosalpha-cosbeta*cosbeta-cosgamma*cosgamma+2*(cosalpha*cosbeta+cosbeta*cosgamma+cosgamma*cosalpha)-2*(cosalpha+cosbeta+cosgamma)+3));
+    return 57.29577951*atan2(sinthetap,sqrt(1-sinthetap*sinthetap));
+}
---
+++ pyramidalization.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+### This script can extract pyramidalization angles from xyz-format files. The 
+### output is pure text and can be easily used by other programs, such as 
+### gnuplot. This script relies on an awk script written by me: pyramidalization
+### .awk. The awk script should be put in the same directory as this script. 
+### This script processes each file with a .xyz file name extension in the wor-
+### king directory. The definition of pyramidalization angle can be found at 
+### J. Comput. Chem., 33: 2173-2179. https://doi.org/10.1002/jcc.23053
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+help() {
+    awk -F'### ' '/^###/ { print $2 }' "$0"
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    help
+    exit 1
+fi
+
+test -s $script_dir/pyramidalization.awk
+if [ $? -ne 0 ]; then
+    echo "Missing pyramidalization.awk!"
+    exit 1
+fi
+echo "This script will execute on all of the xyz-format files in the current directory."
+echo "If you want to skip any step, type q."
+printf "\n"
+pf=1
+while [ $pf -ne 0 ]
+do
+    printf "Type in the pyramidalization angles you need. Example inputs are shown below. If you are not interested in any pyramidalization angles, type in q to proceed to the next step.\n1-3-6-2 7-8-9-10\nThe example input will produce two pyramidalization angles in the output. The central carbon atom of the first pyramidalization angle is the 1st atom. The central carbon atom of the second pyramidalization angle is the 7th atom. The 1st atom is bonded to the 3rd atom, the 6th atom, and the 2nd atom respectively. The 7th atom is bonded to the 8th atom, the 9th atom, and the 10th atom respectively.\n"
+    read -a pyramidalization
+# convert the array to a string
+    pl=$( IFS='|'; echo "${pyramidalization[*]}")
+    if [ "${pyramidalization[0]}" = "q" ]; then
+        pf=0
+        np=0
+    else
+        pf=`echo $pl | awk 'BEGIN { FS="-"; RS="|"; flag=0 } { if(NF!=4) { flag=1; exit 1 } } END { print flag }'`
+        if [ $pf -eq 0 ]; then
+            np=${#pyramidalization[@]}
+        else
+            echo "Wrong input!"
+        fi
+    fi
+done
+if [ $np -ne 0 ]; then
+# replace '-' with space and get a new string to be used by awk
+    pg=${pyramidalization[@]//-/ }
+else
+    pg=0
+fi
+for file in *.xyz
+do
+    target=${file%.*}
+    if (test -a ./$target.pa) then
+        printf "Move %s.pa out of the current directory before running this script." "$target"
+        continue
+    fi
+    num_atom=`head -n 1 $file`
+    if [ "$pg" != "0" ]; then
+        pm=($pg)
+        pz=${pm[0]}
+        for (( c=1; c<${#pm[@]}; c++ ))
+        do
+            if [ ${pm[c]} -gt $pz ]; then
+                pz=${pm[c]}
+            fi	
+        done
+        if [ $pz -gt $num_atom ]; then
+            printf "There are only %d atoms in %s.\n" "$num_atom" "$file"
+            continue
+        fi
+    fi
+    awk -f $script_dir/pyramidalization.awk -v p="$pg" $file $file > $target.pa
+done
